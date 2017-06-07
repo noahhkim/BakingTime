51803104b297dd3b48db54f71dd08522a92cebd5
noahhkim <noahkim03@gmail.com>
2017-06-07 01:36:32 -0700

    implement espresso testing

--- app/src/androidTest/java/com/example/noahkim/bakingtime/MainActivityIntentTest.java
+++ app/src/androidTest/java/com/example/noahkim/bakingtime/MainActivityIntentTest.java
@@ -3,22 +3,24 @@ package com.example.noahkim.bakingtime;
 import android.app.Activity;
 import android.app.Instrumentation.ActivityResult;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.example.noahkim.bakingtime.ui.MainActivity;
+import com.example.noahkim.bakingtime.ui.RecipeDetailsActivity;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Created by Noah on 6/3/2017.
@@ -29,21 +31,24 @@ public class MainActivityIntentTest {
     private final String PACKAGE_NAME = "com.example.noahkim.bakingtime.ui.RecipeDetailsActivity";
 
     @Rule
-    public IntentsTestRule<MainActivity> mActivityRule =
-            new IntentsTestRule<>(MainActivity.class);
+    public IntentsTestRule<RecipeDetailsActivity> mActivityRule =
+            new IntentsTestRule<>(RecipeDetailsActivity.class);
 
-//    @Before
-//    public void stubAllExternalIntents() {
-//        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
-//        // every test run. In this case all external Intents will be blocked.
-//        intending(not(isInternal())).respondWith(new ActivityResult(Activity.RESULT_OK, null));
-//    }
+    @Before
+    public void stubAllExternalIntents() {
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(new ActivityResult(Activity.RESULT_OK, null));
+    }
 
     @Test
-    public void clickRecipeOpensDetails() {
+    public void clickRecipeDetailsOpensStepsActivity() {
+
+        SystemClock.sleep(1000);
+
         // Build a result to return when a particular activity is launched.
         Intent intent = new Intent();
-        intent.putExtra(MainActivity.RECIPE_DETAILS, 2);
+        intent.putExtra(RecipeDetailsActivity.STEP_DETAILS, 2);
         ActivityResult result = new ActivityResult(Activity.RESULT_OK, intent);
 
         // Set up result stubbing when an intent sent to "details" is seen.
@@ -51,9 +56,7 @@ public class MainActivityIntentTest {
 
         // User action that results in details activity being launched.
         // Launching activity expects recipe name to be returned and displays it on the screen.
-        onView(withId(R.id.recipe_list_frame)).perform(click());
+        onView(withId(R.id.recyclerview_steps_list)).perform(click());
 
-        // Assert that data we set up above is shown.
-        onView(withId(R.id.recipe_details_frame)).check(matches(withText(2)));
     }
 }
--- app/src/androidTest/java/com/example/noahkim/bakingtime/MainActivityScreenTest.java
+++ app/src/androidTest/java/com/example/noahkim/bakingtime/MainActivityScreenTest.java
@@ -1,5 +1,6 @@
 package com.example.noahkim.bakingtime;
 
+import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -11,8 +12,11 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 /**
@@ -27,11 +31,13 @@ public class MainActivityScreenTest {
 
     @Test
     public void MainActivityTest() {
+
+        SystemClock.sleep(1000);
+
         onView(withId(R.id.recyclerview_recipes))
                 .perform(actionOnItemAtPosition(0, click()));
 
-//        onView(withId(R.id.ingredient_name))
-//                .check(matches(isDisplayed()));
-
+        onView(withId(R.id.toolbar))
+                .check(matches(hasDescendant(withText("Nutella Pie"))));
     }
 }
